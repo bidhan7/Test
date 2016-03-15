ee888d8b4555bb49ad8b727ed0e227ffdf29258c
bidhan7 <bidhan.subedi7@gmail.com>
2016-03-15 10:50:08 +0545

    First Commit

---
+++ test/src/test/testing.java
@@ -0,0 +1,12 @@
+public class testing{
+	
+	public static void main (String[] args){
+		
+		for (int x = 1; x <= 5; x++){
+			for (int y = 1; y <= x; y++){
+				System.out.print("x");
+			}
+			System.out.println("");
+		}
+	}
+}
